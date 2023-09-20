e9d9b6fad8c6130130d003d4662f94ac91fef114
adammeg <adam.bhedj13@gmail.com>
2023-09-20 16:12:11 +0100

    test

--- controllers/contractFunctions.js
+++ controllers/contractFunctions.js
@@ -41,7 +41,6 @@ const besuRpcUrl = 'http://192.168.1.56:8545'; // Replace with your Besu RPC URL
 
 // Create a Web3 instance
 const web3 = new Web3(besuRpcUrl);
-const contractAbi = ABI
 const contractAddress = "0x51Fe1C18d22027227418083179a12a8f84fD0F88"
 // Create a contract instance
 const contract = new web3.eth.Contract(contractAbi, contractAddress);
@@ -55,10 +54,28 @@ const fromAddress = '0xYourAddress'; // Replace with your Ethereum address
 
 exports.contractCall = async (req, res) => {
     try {
-        const functionName = "retrieve";
-        const result = await contract.methods[functionName]().call();
-        res.status(200).json({ result });
-        console.log("Retrieved value:", result);
+        async function getValueAtAddress(nodeName="node", address, contractAbi, fromPrivateKey, fromPublicKey, toPublicKey) {
+            const web3 = new Web3('http://192.168.1.56:8545')
+            const web3quorum = new Web3Quorum(web3, 1399);
+            const contractAbi = ABI
+            const contract = new web3quorum.eth.Contract(contractAbi);
+            // eslint-disable-next-line no-underscore-dangle
+            const functionAbi = contract._jsonInterface.find(e => {
+              return e.name === "get";
+            });
+            const functionParams = {
+              to: "0x7F36C78727aB7DeF49D3e1B209d5de93db42c5A6",
+              data: functionAbi.signature,
+              privateKey: fromPrivateKey,
+              privateFrom: fromPublicKey,
+              privateFor: [toPublicKey]
+            };
+            const transactionHash = await web3quorum.priv.generateAndSendRawTransaction(functionParams);
+            // console.log(`Transaction hash: ${transactionHash}`);
+            const result = await web3quorum.priv.waitForTransactionReceipt(transactionHash);
+            console.log("" + nodeName + " value from deployed contract is: " + result.output);
+            return result;
+          };
 
     } catch (error) {
         console.log("Error:", error);
